d16f017377afdf9f96259e2ce76299a20fd26e35
hilfing <indradip.paul@outlook.com>
2021-06-28 13:29:56 +0530

    Made it dynamic
    Made it dynamic

--- src/BrainshopChatbotAPI/chatbasics.py
+++ src/BrainshopChatbotAPI/chatbasics.py
@@ -1,17 +1,23 @@
-
 import requests
 
 
 URL = "http://api.brainshop.ai/get"
 
 
-brain = "156099"
-apikey = "4TG9iu82pFOu9XjD"
-uiid = "JarvisAI"
-msg = "Hi"
+brain = ""
+apikey = ""
+uiid = ""
+msg = ""
 
+def setup(brainid, apikeyuser, uiiduser):
+    brain = brainid
+    apikey = apikeyuser
+    uiid = uiiduser
 
 def sendmsg(message1):
+    if brain == "156099":
+        print("This BrainID is reserved. Pls use setup() to set your own id.")
+        return "Reserved BrainID"
     msg = message1
     PARAMS = {
         'bid': brain,
