245439c960c226dddced4fd14ce448dd0a82bff2
hilfing <indradip.paul@outlook.com>
2021-06-28 11:29:18 +0530

    Initial Commit

---
+++ package/src/chatbotapi/chatbot.py
@@ -0,0 +1,28 @@
+
+import requests
+import tkinter
+from tkinter import *
+
+
+
+URL = "http://api.brainshop.ai/get"
+
+
+brain = "156099"
+apikey = "4TG9iu82pFOu9XjD"
+uiid = "JarvisAI"
+msg = "Hi"
+
+
+def sendmsg(message1):
+    msg = message1
+    PARAMS = {
+        'bid': brain,
+        'key': apikey,
+        'uid': uiid,
+        'msg': msg
+    }
+    data = requests.get(url=URL, params=PARAMS).json()['cnt']
+    return data
+
+
