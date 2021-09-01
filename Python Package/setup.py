ff6ea4506fc8c44fab975bb5e1768613537e489c
HilFing <81067543+hilfing@users.noreply.github.com>
2021-09-01 17:08:17 +0530

    New function(getcreds)

--- Python Package/setup.py
+++ Python Package/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ChatbotAPI",
-    version="1.2.2",
+    version="1.3.0",
     author="HilFing",
     author_email="indradip.paul@outlook.com",
     description="API handler to make AI chatbot",
