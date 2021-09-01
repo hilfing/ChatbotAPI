1f6d9141eaa56e8c92e5e43fbf33a87a9b5a0f9b
hilfing <indradip.paul@outlook.com>
2021-09-01 16:41:42 +0530

    Added Jokes and Readme

--- NodeJS Package/index.js
+++ NodeJS Package/index.js
@@ -1,17 +1,23 @@
 const axios = require('axios');
+var Joke = require('one-liner-joke');
 const errors = require('./error');
 const brainshopurl = "http://api.brainshop.ai/get"
 
 let bid = "undefined";
 let apikey = "undefined";
+let jokefilter = {'exclude_tags': ['dirty', 'racist', 'marriage', 'sex']}
 const getcreds = function () {
     let as = "Brain ID : " + bid + " \nAPIKey : " + apikey;
     return as;
 }
 const sendmsg = async function (message,uiid = "ChatBot") {
     try {
+        message = message.toLowerCase()
         if (message === "" || message == null) {
-            throw new errors.NoMessagePassed()
+            throw new errors.NoMessagePassed();
+        }
+        if (message.includes("joke")) {
+            return Joke.getRandomJoke(jokefilter).body;
         }
         params = {
             'bid' : bid,
@@ -22,7 +28,7 @@ const sendmsg = async function (message,uiid = "ChatBot") {
         const response = await axios.get(brainshopurl,{params});
         return response.data.cnt;
     } catch (error) {
-        console.error(error)
+        console.error(error);
         return error;
     }
 }
