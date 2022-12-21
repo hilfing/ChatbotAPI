5606fb1c924ea2542dadc663da7beca5b75edefe
HilFing <81067543+hilfing@users.noreply.github.com>
2022-12-21 14:36:29 +0530

    Update test.js

--- NodeJS Package/test.js
+++ NodeJS Package/test.js
@@ -1,5 +1,5 @@
 const lol = require('./index')
-lol.chatbotsetup('156099', '4TG9iu82pFOu9XjD')
+lol.chatbotsetup('xxx', 'xxx')
 lol.getcreds()
 lol.sendmsg('hi', 'test').then((res) => {
   console.log(res) // Print the contest data on the console
